2d88858290e36f6057e9158239b6c362006ca032
Ida Ebkes <contact@ida-ebkes.eu>
2018-08-29 10:04:29 +0200

    Remoove unnecessary test.

--- Products/GenericSetup/tests/test_tool.py
+++ Products/GenericSetup/tests/test_tool.py
@@ -1732,8 +1732,8 @@ class SetupToolTests(FilesystemTestBase, TarballTester, ConformsToISetupTool):
         # profile_info does contain dependencies, but one of them doesn't exist:
         self._makeFile(METADATA_XML, _BROKEN_METADATA_XML)
         self.assertEqual(tool.dependenciesExist('other:foo'), False)
-        # profile_info does not contain dependencies-property:
-        self.assertRaises(KeyError, tool.getProfileInfo('bar')['dependencies'])
+
+
 _DEFAULT_STEP_REGISTRIES_EXPORT_XML = ("""\
 <?xml version="1.0"?>
 <export-steps>
