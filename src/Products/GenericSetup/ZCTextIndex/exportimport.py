5dd24824d120759b1587ea16f8e91c6e2e519594
Michael Howitz <mh@gocept.com>
2021-01-21 11:23:20 +0100

    Apply isort rules.

--- src/Products/GenericSetup/ZCTextIndex/exportimport.py
+++ src/Products/GenericSetup/ZCTextIndex/exportimport.py
@@ -16,11 +16,10 @@
 from BTrees.IOBTree import IOBTree
 from BTrees.Length import Length
 from BTrees.OIBTree import OIBTree
-from zope.component import adapts
-
 from Products.ZCTextIndex.interfaces import IZCLexicon
 from Products.ZCTextIndex.interfaces import IZCTextIndex
 from Products.ZCTextIndex.PipelineFactory import element_factory
+from zope.component import adapts
 
 from ..interfaces import ISetupEnviron
 from ..utils import NodeAdapterBase
