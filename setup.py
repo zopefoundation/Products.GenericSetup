bb79ab4fc71791cf0aed7e3d1a50b2545c1695a0
Gil Forcada Codinachs <gil.gnome@gmail.com>
2025-03-01 13:17:10 +0100

    chore: adjust dependencies

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
     zip_safe=False,
     python_requires='>=3.8',
     install_requires=[
+        'packaging',
         'setuptools',
         'Zope >= 4.0b4',
         'five.localsitemanager',
